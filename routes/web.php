96ed16dca6067a3d5e066fdccb7fe6d941b154bd
kindlin2012 <kindlin2012@139.com>
2023-08-01 12:05:33 +0000

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root')
+Route::get('/', 'PagesController@root')->name('root');
 // ->middleware('verified');
 
 Auth::routes(['verify' => true]);
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
+Route::group(['middleware' => ['auth', 'verified']], function() {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
