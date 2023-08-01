a9160242af5485fa565ca67667918ff26541d7b0
kindlin2012 <kindlin2012@139.com>
2023-08-01 03:32:48 +0000

    邮箱验证功能

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'PagesController@root')->name('root')
+// ->middleware('verified');
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
