c80887dd27929ce56d3112cf290be8d49a1daca8
kindlin2012 <kindlin2012@139.com>
2023-08-03 19:33:38 +0000

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,12 @@ use App\Models\Product;
 //Route::get('/', 'PagesController@root')->name('root');
 // ->middleware('verified');
 
-Route::redirect('/', '/products')->name('root');
-Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
+
 
 
 Auth::routes(['verify' => true]);
+Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
@@ -35,4 +35,9 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 });
+
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
