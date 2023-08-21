4df5da683fa76a8539d6c1a34b9f8a6cfcfc9ed1
kindlin2012 <kindlin2012@139.com>
2023-08-21 17:15:07 +0000

    顶部导航加入商品类目树

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,10 +7,13 @@ use App\Models\Product;
 use App\Exceptions\InvalidRequestException;
 use App\Models\OrderItem;
 use App\Models\Category;
+use App\Services\CategoryService;
 
 class ProductsController extends Controller
 {
-    public function index(Request $request)
+    // public function index(Request $request)
+    // 使用 Laravel 的依赖注入，自动创建 $categoryService 对象
+    public function index(Request $request, CategoryService $categoryService)
     {
         // $products = Product::query()->where('on_sale', true)->paginate(16);
 
@@ -60,6 +63,7 @@ class ProductsController extends Controller
         }
 
         $products = $builder->paginate(16);
+        // return $categoryService->getCategoryTree();
 
         return view('products.index', [
             'products' => $products,
@@ -69,6 +73,8 @@ class ProductsController extends Controller
             ],
             // 等价于 isset($category) ? $category : null
             'category' => $category ?? null,
+             // 将类目树传递给模板文件
+             'categoryTree' => $categoryService->getCategoryTree(),
         ]);
     }
 
