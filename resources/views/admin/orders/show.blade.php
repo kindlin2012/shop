bf8cc7a9870d3883d481b6805fcf1bce9128690f
kindlin2012 <kindlin2012@139.com>
2023-08-22 07:50:11 +0000

    订单模块调整

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -50,6 +50,9 @@
     <!-- 订单发货开始 -->
     <!-- 如果订单未发货，展示发货表单 -->
     @if($order->ship_status === \App\Models\Order::SHIP_STATUS_PENDING)
+    @if($order->refund_status !== \App\Models\Order::REFUND_STATUS_SUCCESS &&
+          ($order->type !== \App\Models\Order::TYPE_CROWDFUNDING ||
+            $order->items[0]->product->crowdfunding->status === \App\Models\CrowdfundingProduct::STATUS_SUCCESS))
     <tr>
       <td colspan="4">
         <form action="{{ route('admin.orders.ship', [$order->id]) }}" method="post" class="form-inline">
@@ -77,6 +80,7 @@
         </form>
       </td>
     </tr>
+    @endif
     @else
     <!-- 否则展示物流公司和物流单号 -->
     <tr>
@@ -89,6 +93,7 @@
     <!-- 订单发货结束 -->
 
     @if($order->refund_status !== \App\Models\Order::REFUND_STATUS_PENDING)
+
     <tr>
       <td>退款状态：</td>
       <td colspan="2">{{ \App\Models\Order::$refundStatusMap[$order->refund_status] }}，理由：{{ $order->extra['refund_reason'] }}</td>
