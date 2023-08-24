7ab49bfbd270d7fa71f29cde9afdf5d456ebb85c
kindlin2012 <kindlin2012@139.com>
2023-08-24 04:09:01 +0000

    计算逾期费

--- app/Models/Installment.php
+++ app/Models/Installment.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Moontoast\Math\BigNumber;
+
 
 class Installment extends Model
 {
